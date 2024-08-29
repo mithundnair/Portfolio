190a9a53b1fbf4ad08a05d55b3d3d325e43a71ee
mithundnair <mithudileep1234567@gmail.com>
2024-08-29 12:34:53 +0530

    social-icons

---
+++ script.js
@@ -0,0 +1,8 @@
+function View() {
+    document.getElementById('btn').innerHTML = 'You are looking at it';
+
+    setTimeout(function() {
+        btn.innerHTML = 'View Portfolio';
+      }, 3000);
+    }
+  
