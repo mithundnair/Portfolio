8eb9e917cb8e52ad5a8cd1dcb92990c36ea58cfc
mithundnair <mithudileep1234567@gmail.com>
2024-09-03 20:27:42 +0530

    nav update

--- script.js
+++ script.js
@@ -13,4 +13,11 @@ function Vieww() {
         btn1.innerHTML = 'View on Github';
       }, 3000);
     }
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+  
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');  // Toggle the hidden class to show or hide the menu
+  });
   
